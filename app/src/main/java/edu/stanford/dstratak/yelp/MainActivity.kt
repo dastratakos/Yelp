8706a25408b822265c0bc6beda74d06708a42412
Dean Stratakos <dastratakos@gmail.com>
2020-11-04 13:45:01 -0800

    Create basic working version of the app
    The app displays a single static list of 20 restaurants serving Avocado
    Toast in New York. Each item in the list contains various properties of
    the restaurant including name, rating, number of reviews, address,
    category, distance, price range, and an image.

--- app/src/main/java/edu/stanford/dstratak/yelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/dstratak/yelp/MainActivity.kt
@@ -2,10 +2,50 @@ package edu.stanford.dstratak.yelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "lR-eskg2rr8aNV_HMW5N3v8XSSME8IwuPSAPQtGklX-jx4eNkx36OxdOdOiSxbCNrzDv" +
+        "HMnXam73Hfqhizx5bwLoL-j1pIoiAW5wrZAr9m1CQfY2Ngxv33JYB4SiX3Yx"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantsAdapter(this, restaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
+        val retrofit =
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York")
+            .enqueue(object: Callback<YelpSearchResult> {
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if (body == null) {
+                    Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/edu/stanford/dstratak/yelp/YelpDataClasses.kt
@@ -0,0 +1,35 @@
+package edu.stanford.dstratak.yelp
+
+import com.google.gson.annotations.SerializedName
+
+// NOTE: use @SerializedName when the name of the key does not match the name of the local variable
+
+data class YelpSearchResult (
+    val total: Int,
+    @SerializedName("businesses") val restaurants: List<YelpRestaurant>
+)
+
+data class YelpRestaurant (
+    val name: String,
+    val rating: Double,
+    val price: String,
+    @SerializedName("review_count") val numReviews: Int,
+    @SerializedName("distance") val distanceInMeters: Double,
+    @SerializedName("image_url") val imageUrl: String,
+    val categories: List<YelpCategory>,
+    val location: YelpLocation
+) {
+    fun displayDistance(): String {
+        val milesPerMeter = 0.000621371
+        val distanceInMiles = "%.2f".format(distanceInMeters * milesPerMeter)
+        return "$distanceInMiles mi"
+    }
+}
+
+data class YelpCategory (
+    val title: String
+)
+
+data class YelpLocation(
+    @SerializedName("address1") val address: String
+)
