e8b3753ac29a8f2fa146e9e2072dc95acfe878d5
Dean Stratakos <dastratakos@gmail.com>
2020-11-04 17:32:01 -0800

    Add dividers between restaurants

--- app/src/main/java/edu/stanford/dstratak/yelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/dstratak/yelp/MainActivity.kt
@@ -1,8 +1,9 @@
 package edu.stanford.dstratak.yelp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -10,6 +11,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 private const val TAG = "MainActivity"
 private const val BASE_URL = "https://api.yelp.com/v3/"
@@ -26,28 +28,37 @@ class MainActivity : AppCompatActivity() {
         val adapter = RestaurantsAdapter(this, restaurants)
         rvRestaurants.adapter = adapter
         rvRestaurants.layoutManager = LinearLayoutManager(this)
+        rvRestaurants.addItemDecoration(
+            DividerItemDecoration(
+                rvRestaurants.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
 
         val retrofit =
             Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
             .build()
         val yelpService = retrofit.create(YelpService::class.java)
         yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York")
-            .enqueue(object: Callback<YelpSearchResult> {
-            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
-                Log.i(TAG, "onResponse $response")
-                val body = response.body()
-                if (body == null) {
-                    Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
-                    return
+            .enqueue(object : Callback<YelpSearchResult> {
+                override fun onResponse(
+                    call: Call<YelpSearchResult>,
+                    response: Response<YelpSearchResult>
+                ) {
+                    Log.i(TAG, "onResponse $response")
+                    val body = response.body()
+                    if (body == null) {
+                        Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
+                        return
+                    }
+                    restaurants.addAll(body.restaurants)
+                    adapter.notifyDataSetChanged()
                 }
-                restaurants.addAll(body.restaurants)
-                adapter.notifyDataSetChanged()
-            }
 
-            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
-                Log.i(TAG, "onFailure $t")
-            }
-        })
+                override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                    Log.i(TAG, "onFailure $t")
+                }
+            })
 
     }
 }
